7cb94ac03a1a2ed0a9a54662e2f81d5e5d257fa3
Alith <alithferr95@gmail.com>
2024-07-28 22:58:18 -0500

    funciona  el usuer y el pw

--- js/logica.js
+++ js/logica.js
@@ -1,3 +1,20 @@
 
+console.log('logica conectada');
 
-console.log ('logica conectada');
+const usuario = 'alith'
+const contraseña = '12345'
+
+function pruebaBoton() {
+
+    let user = document.getElementById('user').value
+    let pw = document.getElementById('pw').value
+
+    if(usuario==user && contraseña==pw){
+     console.log('ingreso correcto');
+
+     } else {
+     console.log('contraseña incorrecta');
+        
+     }
+
+}
